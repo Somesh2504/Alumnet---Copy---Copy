2c4c1008142e78ab83afe75b3a91d904a999bd53
Somesh2504 <someshchevula25@gmail.com>
2025-09-15 20:30:38 +0530

    url changes

--- Backend/server.js
+++ Backend/server.js
@@ -35,7 +35,7 @@ app.get('/',(req,res)=>{
 
 
 app.use(cors({
-  origin: ["https://alumnet-frontend-c0cg.onrender.com", "http://localhost:5173", "http://localhost:3000"],
+  origin: ["https://alumnet-frontend-c0cg.onrender.com", "http://localhost:5173", "http://localhost:3000","https://alumnet-frontend.onrender.com"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
